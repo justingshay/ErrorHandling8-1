aa516fa88affb004e1d6a241a245d018fcfae9dd
justingshay <justin.g.shay@gmail.com>
2024-09-19 01:32:05 -0400

    Edited the file to print out vacation days/mission and total accrued vacation days.

--- detective.js
+++ detective.js
@@ -11,3 +11,22 @@ function mysteryOperation ()
 		throw new Error("The operation is failed mysteriously!");
 	}
 }
+
+var vDays = 0;
+const totalMissions = 20;
+const opSuccessVacationDays = 13;
+const opFailedVacationDays = 1;
+const opAttendanceVacationDays = 3;
+for(let i = 0; i < totalMissions; i++){
+	try{
+		console.log("- - - - - - - - - - -\nOperation number " + (i + 1) + ". Let's go!");
+		mysteryOperation();
+		vDays += (opSuccessVacationDays + opAttendanceVacationDays);
+		console.log("Woohoo! I just accrued " + (opSuccessVacationDays + opAttendanceVacationDays) + " vacation days!");
+	} catch {
+		vDays += (opFailedVacationDays + opAttendanceVacationDays);
+		console.log("Ah man, operation failed. At least I still accrued " + (opFailedVacationDays + opAttendanceVacationDays) + " vacation days.");
+	}
+	console.log("- - - - - - - - - - -");
+}
+console.log("Oh man, after 20 missions I deserve " + vDays + " vacation days.");
